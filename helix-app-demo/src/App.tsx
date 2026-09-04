16d3bde3c5a6094e530967ac9bb4da16a40d7c3e
Daniel Sanchez <hd2242kin20@gmail.com>
2026-09-04 02:11:11 -0300

    Verificação de Autenticação criado

--- helix-app-demo/src/App.tsx
+++ helix-app-demo/src/App.tsx
@@ -1,17 +1,19 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Login from './pages/Auth/Login'
 import Perfil from './pages/Perfil/Perfil'
 import PortalMedico from './pages/PortalMedico/PortalMedico'
 import Cadastro from './pages/Cadastro/Cadastro'
+import { useAuth } from './hooks/useAuth'
 
 import './App.css'
 
 function App() {
+  const {isAuthenticated} = useAuth()
 
   return (
      <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Login/>}/>
+            <Route path='/' element={isAuthenticated ? <Navigate to="/perfil" /> : <Login />}/>
             <Route path='/perfil' element={<Perfil/>}/>
             <Route path='/portal-medico' element={<PortalMedico/>}/>
             <Route path='/cadastro' element={<Cadastro/>}/>
--- helix-app-demo/src/pages/Perfil/Perfil.tsx
+++ helix-app-demo/src/pages/Perfil/Perfil.tsx
@@ -1,7 +1,7 @@
 function Perfil() {
   return (
     <div>
-        Oi
+        Perfil
     </div>
   )
 }
