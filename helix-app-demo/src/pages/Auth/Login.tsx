3b22d78469da8c2ae4caab8b863a3824210a15ae
Daniel Sanchez <hd2242kin20@gmail.com>
2026-09-08 18:13:52 -0300

    Mudar placeholder

--- helix-app-demo/src/pages/Auth/Login.tsx
+++ helix-app-demo/src/pages/Auth/Login.tsx
@@ -33,7 +33,7 @@ export default function Login() {
               value={cpfEmail}
               onChange={(e) => setCpfEmail(e.target.value)}
               className="w-full bg-neutral-800 border border-neutral-700 rounded-lg px-3 py-2 text-sm focus:outline-none focus:border-helix-green"
-              placeholder="ana.carolina@email.com"
+              placeholder="seu.nome@endereco.com"
             />
           </div>
 
